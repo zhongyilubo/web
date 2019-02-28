c95825110580ad602e780ba3554eb99980cfcd4f
duanzhiwei <89340545@qq.com>
2019-02-28 22:58:52 +0800

    df

--- app/helpers.php
+++ app/helpers.php
@@ -5,14 +5,14 @@
  * Date: 2019/2/28
  * Time: 22:47
  */
+
 /**
  * 获取域名
  */
 if(!function_exists('get_domain')){
-    function get_domain($name)
-    {
-        $url = env('APP_URL');
-        $suffix = strpos($url, 'localhost') ? '.cw100.la' : strstr($url, '.');
-        return $name=='web' ? 'www'.$suffix : strtolower($name).$suffix;
+    function get_domain($name){
+        $url = config('app.url');
+        $suffix = strstr($url, '.');
+        return strtolower($name).$suffix;
     }
 }
