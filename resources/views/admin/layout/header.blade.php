75666f659323b07a44cc105bb80f4a5bb4b8b241
duanzhiwei <89340545@qq.com>
2019-03-02 14:03:38 +0800

    df

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -11,6 +11,5 @@
     <link rel="stylesheet" href="/admin/css/flex.css">
     <link rel="stylesheet" href="/admin/css/ModifyNew.css">
     <script src="/vendor/seajs/1.3.1/sea.js" id="seajsnode"></script>
-    <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
     @yield('style')
 </head>
