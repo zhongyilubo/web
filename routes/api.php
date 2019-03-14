a67c603fe8ed3652d8c576f9ea019a5c4d3c8003
duanzhiwei <89340545@qq.com>
2019-03-15 07:53:46 +0800

    df

--- routes/api.php
+++ routes/api.php
@@ -10,8 +10,13 @@ use Illuminate\Http\Request;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
-|
+|https://learnku.com/docs/dingo-api/2.0.0/Errors-And-Error-Responses/1447
 */
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', function ($api) {
+    $api->get('users/{id}', 'App\Api\V1\Controllers\UserController@show');
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
