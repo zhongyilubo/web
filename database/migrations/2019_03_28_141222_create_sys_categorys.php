80cb4b4941563e063250e2209a49b6d8a704b705
duanzhiwei <89340545@qq.com>
2019-03-29 10:24:26 +0800

    df

--- database/migrations/2019_03_28_141222_create_sys_categorys.php
+++ database/migrations/2019_03_28_141222_create_sys_categorys.php
@@ -19,10 +19,15 @@ class CreateSysCategorys extends Migration
             $table->string('image',160)->default('')->comment('分类图片');
             $table->integer('sorts')->default(0);
             $table->integer('parent_id')->default(0);
-            $table->integer('category_id');
-            $table->string('category_type',160);
+            $table->tinyInteger('type')->default(0)->comment('备注见模型');
             $table->tinyInteger('status')->default(2)->comment('1正常 2冻结');
             $table->timestamps();
+        });
+
+        Schema::create('sys_categoryables', function (Blueprint $table) {
+            $table->integer('category_id');
+            $table->integer('categoryable_id');
+            $table->string('categoryable_type',160);
         });
     }
 
