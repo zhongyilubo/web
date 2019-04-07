728c7ff283853bb265c11db8ef95d430afa2da60
duanzhiwei <89340545@qq.com>
2019-04-07 21:56:52 +0800

    df

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -9,9 +9,18 @@
 namespace App\Http\Controllers\Api;
 
 
+use App\Models\Gds\GdsGood;
+
 class IndexController extends InitController
 {
     public function index(){
-        return ['sdfsd' => 1234];
+        $conf = @file_get_contents('banner.txt');
+        $banner = $conf ? json_decode($conf,true):[];
+
+        return $this->response->array([
+            'banner' => $banner,
+            'hot'   => GdsGood::orderBy('sorts','desc')->take(3)->get(),
+            'new'   => GdsGood::orderBy('created_at','desc')->take(6)->get(),
+        ]);
     }
 }
